66dc10b868df7d06fe5b680900b39cf69f6e467d
UZA97 <81206899+UZA97@users.noreply.github.com>
2022-02-20 01:08:28 +0900

    #Edit LoginMusic

--- Assets/Scripts/InGame/Button.cs
+++ Assets/Scripts/InGame/Button.cs
@@ -33,14 +33,11 @@ public class Button : MonoBehaviour
         if(Input_UserName.text == "" || !nameCount) {
             Notificationtext.text = "이름이 비어있거나\n이름이 옳바르지 않아요!!";
             // Notificationtext.text = "There are\ncharacters unavailable.";
-
-
             return;
         }
         else if(!DBManager._instance.mIsVaildName){
             Notificationtext.text = "이미 같은 이름이 있어요!!";
             // Notificationtext.text = "ID Account already exists,\nTry again.";
-
             return;
         }
         else if(DBManager._instance.mIsVaildName && nameCount){
